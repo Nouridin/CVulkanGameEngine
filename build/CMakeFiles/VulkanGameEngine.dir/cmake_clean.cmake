93496738f11879dd903d641363dbe98c68081e59
Nouridin <modi9hofi@gmail.com>
2024-09-27 23:32:55 +0300

    Added rendering loops at main.cpp

---
+++ build/CMakeFiles/VulkanGameEngine.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/VulkanGameEngine.dir/src/main.cpp.o"
+  "CMakeFiles/VulkanGameEngine.dir/src/main.cpp.o.d"
+  "VulkanGameEngine"
+  "VulkanGameEngine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/VulkanGameEngine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,35 @@
 #include <vulkan/vulkan.h>
 #include <iostream>
+#include <GLFW/glfw3.h>
 
+	// Create a Window
+GLFWwindow* createWindow(int width, int height, const char* title) {
+
+	if (!glfwInit()) {
+		std::cerr << "Failed to initialize GLFW!" << std::endl;
+		return nullptr;
+	}
+
+	glfwWidnowHint(GLFW_CLIENT_API, GLFW_NO_API);
+	GLFWwindow* window = glfwCreateWindow(width, height, title, nullptr, nullptr);
+	if (!window) {
+
+		std::cerr << "Failed to create GLFW window!" << std::endl;
+		glfwTerminate();
+		return nullptr;
+
+	}
+	return window;
+}
+
+	
 int main() {
+	// Create a GLFW window
+	GLFWwindow* window = createWindow(800, 600, "Vulkan Window");
+	if (!window) return -1;
 
 	VkInstance instance;
-
+	// Create Instance
 	VkInstanceCreateInfo createInfo = {};
 	createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
 
@@ -12,9 +37,20 @@ int main() {
 		std::cerr << "Failed to create Vulkan instance!" << std::endl;
 		return -1;
 	}
+	
+	// Main rendering loop
+	while (!glfwWindowShouldClose(window)) {
+		// Handle Window events
+		glfwPollEvents();
+
+		std::cout << "Rendering frame..." << std::endl;
+	}
 
 	std::cout << "Vulkan instance created successfully!" << std::endl;
-
+	// Cleanup
+	
 	vkDestroyInstance(instance, nullptr);
+	glfwDestroyWindow(window);
+	glfwTerminate();
 	return 0;
 }
